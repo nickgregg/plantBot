e6dc836932537a2e0a1a0a01a882661cd7e030e1
jfunky <jasminea.soltani@gmail.com>
2018-12-13 16:10:55 -0500

    fix humidity call

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def sensorTimerA():
 
 def humidityTempRead():
     #where 22 is the sensor & 4 is GPIO
-    humidity, temperature = Adafruit_DHT.read_retry(22, 4)
+    humidity, temperature = humidity.Adafruit_DHT.read_retry(22, 4)
 
     if humidity is not None and temperature is not None:
         print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
